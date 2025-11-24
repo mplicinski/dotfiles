e907654c4d4db0cc3101ad3835b793612e530abf
mplicinski <mplicinski@gmail.com>
2025-11-23 23:39:08 -0600

    added bootstrap script to setup symlinks on new systems

---
+++ bootstrap.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+DOTFILES="$HOME/.dotfiles"
+
+link() {
+  src=$1
+  dest=$2
+
+  if [ -e "$dest" ] || [ -L "$dest" ]; then
+    echo "Backing up existing $dest -> $dest.backup"
+    mv "$dest" "$dest.backup"
+  fi
+
+  echo "Linking $src -> $dest"
+  ln -s "$src" "$dest"
+}
+
+echo "=== Setting up symlinks ==="
+
+link "$DOTFILES/zsh/.zshrc" "$HOME/.zshrc"
+link "$DOTFILES/tmux/.tmux.conf" "$HOME/.tmux.conf"
+link "$DOTFILES/git/.gitconfig" "$HOME/.gitconfig"
+
+mkdir -p "$HOME/.config"
+link "$DOTFILES/nvim/.config/nvim" "$HOME/.config/nvim"
+link "$DOTFILES/oh-my-posh/.config/oh-my-posh" "$HOME/.config/oh-my-posh"
+link "$DOTFILES/karabiner/.config/karabiner" "$HOME/.config/karabiner"
+
+echo "Done."
